fd28dd43786c8dea451916d3b551dda76affd0ec
ikoyski <ikoyski@gmail.com>
2026-09-20 00:11:00 +0000

    corrected url

--- src/main/java/com/ikoyki/webtools/kanban/backend/controller/BoardController.java
+++ src/main/java/com/ikoyki/webtools/kanban/backend/controller/BoardController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/v1/board")
+@RequestMapping("/v1/boards")
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
